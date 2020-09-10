cebcbf3bf1e16673f37f993b9e9a499f7a535956
Jay Lampstack <jlampstack@gmail.com>
2020-09-10 11:54:16 -0400

    Cleaned up project for blank slate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'>
-					Hey Ajea!
-				</a>
-			</header>
+			<h1>Hey Team, Hi5!</h1>
 		</div>
 	);
 }
